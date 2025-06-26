b43c23ac81ded9aa7e47b77d0c69ba0be2bcfe08
talhawajdan <talhawajdan@gmail.com>
2025-06-26 22:11:47 +0500

    #update

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/",
-        destination: "/sign-in",
+        destination: "/home",
         permanent: true,
       },
     ];
